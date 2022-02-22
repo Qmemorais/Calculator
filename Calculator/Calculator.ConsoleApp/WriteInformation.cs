36e464e409d99a33c39166df660d47eb544de207
Daria Burtseva <qmemorais@gmail.com>
2022-02-22 01:08:37 -0800

    Create new class to write info to console
    create new class WriteInformation and two public methods
    Write() - to write results to console
    Read() - to read from console new string

---
+++ Calculator/Calculator.ConsoleApp/WriteInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator.ProjectCalculator.ConsoleApp
+{
+    class WriteInformation
+    {
+        public void Write(string toWrite)
+        {
+            Console.WriteLine(toWrite);
+        }
+
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
