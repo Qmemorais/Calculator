dfe742ca5e74f64992e55e43689267e256e77200
Daria Burtseva <qmemorais@gmail.com>
2022-02-22 04:12:16 -0800

    Create first UnitTest to ConsoleApp with Moq
    create first method to test ConsoleApp. Input empty string< output is "0"

--- Calculator/Calculator.ConsoleApp.Tests/ConsoleAppTests.cs
+++ Calculator/Calculator.ConsoleApp.Tests/ConsoleAppTests.cs
@@ -5,17 +5,25 @@ namespace Calculator.ConsoleApp.Tests
 {
     public class ConsoleAppTests
     {
-
-        [SetUp]
-        public void Setup()
-        {
-
-        }
+        Menu menu;
+        Mock<IWriteRead> writeRead = new Mock<IWriteRead>();
+        Mock<CalculatorLogic.ICalculator> calculator = new Mock<CalculatorLogic.ICalculator>();
 
         [Test]
         public void MenuAdd_InputEmptyString_ReturnZero()
         {
-
+            //
+            var readLine = "";
+            var writeLineRes = "Result is: 0";
+            var writeLineNextInput = "You can enter other numbers (enter to exit)";
+            writeRead.Setup(x => x.Read()).Returns(readLine);
+            calculator.Setup(x => x.Add(readLine)).Returns(0);
+            menu = new Menu(writeRead.Object, calculator.Object);
+            //
+            menu.Add();
+            //
+            writeRead.Verify(x => x.Write(writeLineRes), Times.Once);
+            writeRead.Verify(x => x.Write(writeLineNextInput), Times.Once);
         }
     }
 }
