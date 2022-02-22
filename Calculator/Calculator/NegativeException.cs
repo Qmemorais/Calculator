6e337bc0da6b0097a31ddf970548ee184aae67ea
Daria Burtseva <qmemorais@gmail.com>
2022-02-22 00:25:13 -0800

    update return type in NegativeException
    update type to return negative values from List<int> to IEnumerable<int>

--- Calculator/Calculator/NegativeException.cs
+++ Calculator/Calculator/NegativeException.cs
@@ -6,11 +6,11 @@ namespace Calculator.ProjectCalculator
 {
     public class NegativeException : ArgumentException
     {
-        public List<int> Value { get; }
-        public NegativeException(string message, List<int> val)
-            : base(message)
+        public IEnumerable<int> Value { get; }
+
+        public NegativeException(string message, List<int> val) : base(message)
         {
-            Value = val.Where(x => x < 0).ToList();
+            Value = val.Where(x => x < 0);
         }
     }
 }
