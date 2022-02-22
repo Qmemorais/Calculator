9060c8d2b150efe865eb84d123d294438987f8d7
Daria Burtseva <qmemorais@gmail.com>
2022-02-22 00:26:58 -0800

    create class Menu
    create class Menu from ConsoleApp to write method which call Calculator.Add()

---
+++ Calculator/Calculator.ConsoleApp/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculator.ProjectCalculator.ConsoleApp
+{
+    class Menu
+    {
+        readonly Calculator calculator = new Calculator();
+        public void Add()
+        {
+            Console.WriteLine("Enter comma separated numbers (enter to exit):");
+            do
+            {
+                try
+                {
+                    string numbers = Console.ReadLine();
+                    try
+                    {
+                        var result = calculator.Add(numbers);
+                        Console.WriteLine($"Result is: {result}");
+                    }
+                    catch (NegativeException ex)
+                    {
+                        Console.WriteLine("negatives not allowed" +
+                            $"\n{string.Join(", ", ex.Value)}");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("You can enter other numbers (enter to exit)");
+                }
+            } while (true);
+        }
+    }
+}
