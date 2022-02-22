4a59a8ce8019b5ef3e7c9aaffeb0e212704f7951
Daria Burtseva <qmemorais@gmail.com>
2022-02-22 00:28:14 -0800

    Update ConsoleApp Program class
    remove method to work with Calculator.Add to new class "Menu". Add call method Menu.Add() to work with Calculator

--- Calculator/Calculator.ConsoleApp/Program.cs
+++ Calculator/Calculator.ConsoleApp/Program.cs
@@ -6,39 +6,8 @@ namespace Calculator.ProjectCalculator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter comma separated numbers (enter to exit)");
-            Menu();
-        }
-
-        public static void Menu()
-        {
-            Calculator calculator = new Calculator();
-
-            do
-            {
-                try
-                {
-                    string numbers = Console.ReadLine();
-                    try
-                    {
-                        var result = calculator.Add(numbers);
-                        Console.WriteLine($"Result is: {result}");
-                    }
-                    catch (NegativeException ex)
-                    {
-                        Console.WriteLine("negatives not allowed" +
-                            $"\n{string.Join(", ", ex.Value)}");
-                    }
-
-                    Console.WriteLine("You can enter other numbers (enter to exit)");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    Console.WriteLine("You can enter other numbers (enter to exit)");
-                }
-
-            } while (true);
+            Menu menu = new Menu();
+            menu.Add();
         }
     }
 }
