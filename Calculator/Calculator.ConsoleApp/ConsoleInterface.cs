fac1a0c04b62705a35f965a0a3543c476a0246cc
Daria Burtseva <qmemorais@gmail.com>
2022-02-23 00:50:10 -0800

    Update Menu to ConsoleInterface
    update name of class to more readable and understanding what this class is doing. create constructor to initialize it in UnitTests. Create method Start() to start work. Create method CallCalculatorMethodAdd to call method Add from Calculator

---
+++ Calculator/Calculator.ConsoleApp/ConsoleInterface.cs
@@ -0,0 +1,50 @@
+﻿using Calculator.CalculatorLogic;
+
+namespace Calculator.ConsoleApp
+{
+    public class ConsoleInterface
+    {
+        private readonly IConsoleInOut _consoleInOut = new ConsoleInOut();
+        private readonly CalculatorLogic.Calculator _calculator = new CalculatorLogic.Calculator();
+        private string _numbers;
+
+        public ConsoleInterface(IConsoleInOut writeRead, CalculatorLogic.Calculator calculator)
+        {
+            _consoleInOut = writeRead;
+            _calculator = calculator;
+        }
+
+        public ConsoleInterface()
+        {
+
+        }
+
+        public void Start()
+        {
+            _consoleInOut.Write("Enter comma separated numbers (enter to exit):");
+
+            while(true)
+            {
+                CallCalculatorMethodAdd();
+            }
+        }
+
+        public void CallCalculatorMethodAdd()
+        {
+            _numbers = _consoleInOut.Read();
+           
+            try
+            {
+                var result = _calculator.Add(_numbers);
+
+                _consoleInOut.Write($"Result is: {result}");
+            }
+            catch (NegativeException ex)
+            {
+                _consoleInOut.Write($"negatives not allowed \n{string.Join(", ", ex.Value)}");
+            
+            }
+            _consoleInOut.Write("You can enter other numbers (enter to exit)");
+        }
+    }
+}
